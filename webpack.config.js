8468489d97b7536eab9f9e4fd88449e67b6a29ff
Aaqibhafeez Khan <aaqibhafeezkhan@gmail.com>
2024-10-17 22:37:18 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 9000,
+  },
+};
