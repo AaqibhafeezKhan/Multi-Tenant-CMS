b4d59c4e705d92e1074fbd88cbb8c92ab29ef21c
Aaqibhafeez Khan <aaqibhafeezkhan@gmail.com>
2024-10-17 23:03:21 +0530

    Update single-spa.config.js

--- single-spa.config.js
+++ single-spa.config.js
@@ -1,1 +1,27 @@
+import { registerApplication, start } from 'single-spa';
 
+registerApplication({
+  name: '@cms-react-editorial',
+  app: () => System.import('cms-react-editorial'),
+  activeWhen: ['/editorial'],
+});
+
+registerApplication({
+  name: '@cms-vue-media',
+  app: () => System.import('cms-vue-media'),
+  activeWhen: ['/media-library'],
+});
+
+registerApplication({
+  name: '@cms-angular-auth',
+  app: () => System.import('cms-angular-auth'),
+  activeWhen: ['/role-access'],
+});
+
+registerApplication({
+  name: '@cms-svelte-collab',
+  app: () => System.import('cms-svelte-collab'),
+  activeWhen: ['/real-time-editor'],
+});
+
+start();
